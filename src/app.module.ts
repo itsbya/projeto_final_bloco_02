1fee5c65c8cf727c1a6d2c420a3f09f3cdb87ae0
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-03-17 13:29:07 -0300

    :wrench: Configiração inicial concluida

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '7803luxO.',
+      database: 'db_ecommerce_farmacia',
+      entities: [],
+      synchronize: true,
+})
+
+],
   controllers: [AppController],
   providers: [AppService],
 })
